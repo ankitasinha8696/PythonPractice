4ae6f95033168432ae7d3c70fad7707b39caebb4
Ankita Sinha <ankitasinha322@gmail.com>
2025-04-08 20:29:09 +0530

    pandas practice

--- listProblems.py
+++ listProblems.py
@@ -1,9 +1,21 @@
 nested_list = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+nested_list_2 = [1, [2, 3], [4, [5, 6, [7, 8], 9]]]
 
-# Using list comprehension to sum each inner list
-output = [sum(column) for column in zip(*nested_list)]
+# # Using list comprehension to sum each inner list
+# output = [sum(column) for column in zip(*nested_list)]
 
-print(output)
+# print(output)
 
-my_list = [1, 2, 3] + [4, 5] * 2
-print(my_list)
+# my_list = [1, 2, 3] + [4, 5] * 2
+# print(my_list)
+
+def flatten_list(input_list):
+    flat_list = []
+    for item in input_list:
+        if isinstance(item, list):
+            flat_list.extend(flatten_list(item))
+        else:
+            flat_list.append(item)
+    return flat_list
+
+print(flatten_list(nested_list_2))
