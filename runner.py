d91b90558f6d0a3484c77174aea40af127f5e367
Ankita Sinha <ankitasinha322@gmail.com>
2025-04-09 20:32:07 +0530

    runner files changed

--- runner.py
+++ runner.py
@@ -1,10 +1,11 @@
 import os
 
-path = 'C:/Users/z004w44s/Documents/GitHub/PythonPractice'
+path = 'D:/JenkinsProjects/python-practice'
 
 for root, dirs, files in os.walk(path):
     for file in files:
         if file.endswith('.py'):
             filepath = os.path.join(root, file)
             print(f'Running: {filepath}')
-            os.system(f'python "{filepath}"')
+            exit_code = os.system(f'python "{filepath}"')
+            print(f'Exit code: {exit_code}')
